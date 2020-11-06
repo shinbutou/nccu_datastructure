b86ddbc1a88bb5dd1f21b6c4eca33a45f134ed72
mozium <ChengHsi.Yang@gmail.com>
2020-11-06 11:34:17 +0800

    updating codes for hw5

---
+++ Arsenal/mozium/HW5/src/Main.java
@@ -0,0 +1,21 @@
+// I have no idea what this is about
+// package ta; 
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine())
+		{
+			String url = sc.next();
+			HtmlMatcher hm = new HtmlMatcher(url);
+			hm.match();
+		}
+	}
+}
